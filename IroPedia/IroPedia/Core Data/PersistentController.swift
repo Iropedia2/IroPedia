ad2ee41701115e6272176716f0fc8bd42d0f2d40
ryuki0007 <ryuki.ishida.9y@stu.hosei.ac.jp>
2024-11-29 14:18:03 +0900

    na

---
+++ IroPedia/IroPedia/Core Data/PersistentController.swift
@@ -0,0 +1,35 @@
+import CoreData
+
+class PersistentController {
+    static let shared = PersistentController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "MyModelName")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+    }
+
+    var context: NSManagedObjectContext {
+        return container.viewContext
+    }
+
+    func saveContext() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
